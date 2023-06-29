09aa637e296ac60c3824a34fcef78398bfbc064b
FoliageForest <2258893952@qq.com>
2023-06-29 14:50:38 +0800

    Update

--- src/qimo/student/Test.java
+++ src/qimo/student/Test.java
@@ -7,17 +7,13 @@ public class Test {
     public static void main(String[] args) {
         StudentSinglyLinkedList list = new StudentSinglyLinkedList();
         int stuIdInit = 123456000;
-        list.addNodeAtFirst(new Student(++stuIdInit, "name001", 'W', "计应", "2132"));
-        list.addNodeAtFirst(new Student(++stuIdInit, "name002", 'W', "计应", "2132"));
-        list.addNodeAtFirst(new Student(++stuIdInit, "name003", 'W', "计应", "2132"));
-        list.addNodeAtFirst(new Student(++stuIdInit, "name004", 'W', "计应", "2132"));
-        list.addNodeAtFirst(new Student(++stuIdInit, "name005", 'W', "计应", "2132"));
-        list.addNodeAtFirst(new Student(++stuIdInit, "name006", 'W', "计应", "2132"));
-        list.addNodeAtFirst(new Student(++stuIdInit, "name007", 'W', "计应", "2132"));
-        list.addNodeAtFirst(new Student(++stuIdInit, "name008", 'W', "计应", "2132"));
-        list.addNodeAtFirst(new Student(++stuIdInit, "name009", 'W', "计应", "2132"));
-        list.addNodeAtFirst(new Student(++stuIdInit, "name010", 'W', "计应", "2132"));
-
+        Student[] students = new Student[9];
+        for (int i = 0; i < 9; i++) {
+            Student s = new Student(++stuIdInit, "name00" + (i + 1), 'M', "计应", "2132");
+            s.setGrades(new double[]{Math.random() * 100, Math.random() * 100, Math.random() * 100});
+            list.addNodeAtFirst(s);
+        }
+        System.out.println(list.findStudentByName("name001"));
     }
 
 }
